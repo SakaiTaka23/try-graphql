dc6c84fdef5e0dacd321aedd9991e40c4946fd43
SakaiTaka23 <tt3.sakai@gmail.com>
2021-11-23 20:35:31 +0900

    cors パス周りのバグ修正

--- next-nest/backend/src/main.ts
+++ next-nest/backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://studio.apollographql.com'],
+    origin: ['http://127.0.0.1:3000', 'https://studio.apollographql.com'],
     methods: ['GET'],
   });
 
--- next-nest/frontend/pages/_app.tsx
+++ next-nest/frontend/pages/_app.tsx
@@ -3,13 +3,13 @@ import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = new ApolloClient({
-    uri: 'http://127.0.0.1:5000',
+    uri: 'http://127.0.0.1:5000/graphql',
     cache: new InMemoryCache(),
     connectToDevTools: true,
   });
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ApolloProvider>
   );
 }
