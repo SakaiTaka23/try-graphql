ef3182ee1d1b30862d1e1b9f60f2a3ad52447100
SakaiTaka23 <tt3.sakai@gmail.com>
2021-11-23 14:54:59 +0900

    graphqlのコード生成をした

--- next-nest/backend/src/app.module.ts
+++ next-nest/backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AppService } from './app.service';
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       typePaths: ['./**/*.graphql'],
     }),
+    TodosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ next-nest/backend/src/graphql.ts
@@ -0,0 +1,32 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+export class CreateTodoInput {
+    task: string;
+    detail?: Nullable<string>;
+}
+
+export class Todo {
+    id: number;
+    task: string;
+    detail?: Nullable<string>;
+    isCompleted: boolean;
+}
+
+export abstract class IQuery {
+    abstract todos(): Nullable<Todo>[] | Promise<Nullable<Todo>[]>;
+
+    abstract todo(id: number): Nullable<Todo> | Promise<Nullable<Todo>>;
+}
+
+export abstract class IMutation {
+    abstract createTodo(createTodoInput: CreateTodoInput): Todo | Promise<Todo>;
+}
+
+type Nullable<T> = T | null;
