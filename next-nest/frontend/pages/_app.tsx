cd721269560a82e6d790f6d6cf7fc50474e0029a
SakaiTaka23 <tt3.sakai@gmail.com>
2021-11-23 18:25:13 +0900

    apolloの設定をした

--- next-nest/frontend/pages/_app.tsx
+++ next-nest/frontend/pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const client = new ApolloClient({
+    uri: 'http://127.0.0.1:5000',
+    cache: new InMemoryCache(),
+    connectToDevTools: true,
+  });
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
